d1f65415a61d251937b63e302a676a0b287ed135
Nando <sesisnando.neto@creathus.org.br>
2020-11-30 14:13:57 -0400

    Categories List

--- src/main/java/com/nando/Testes/video/aulas/resources/CategoryResource.java
+++ src/main/java/com/nando/Testes/video/aulas/resources/CategoryResource.java
@@ -1,16 +1,32 @@
 package com.nando.Testes.video.aulas.resources;
 
+
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nando.Testes.video.aulas.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String list() {
-		return "REST funf";
+	public List<Category> list() {
+		
+		Category cat1 = new Category(1, "Informatic");
+		Category cat2 = new Category(2, "Office");
+		
+		List<Category> listCategories = new ArrayList<>();
+		
+		listCategories.add(cat1);
+		listCategories.add(cat2);
+		
+		return listCategories;
 	}
 
 }
