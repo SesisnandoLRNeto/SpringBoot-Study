dc42434fefc9f2c0251bee25b75e1057daf40d92
SesisnandoLRNeto <sesisnando.neto@apoemafabsoft.com>
2020-11-29 21:23:58 -0400

    Initialer Project to Spring Boot

---
+++ src/main/java/com/nando/Testes/video/aulas/TestesVideoAulasApplication.java
@@ -0,0 +1,13 @@
+package com.nando.Testes.video.aulas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestesVideoAulasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestesVideoAulasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nando/Testes/video/aulas/TestesVideoAulasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nando.Testes.video.aulas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestesVideoAulasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
