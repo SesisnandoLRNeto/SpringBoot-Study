70a35914fb7f2ac6e330a99c41c4f706b8f78413
Nando <sesisnando.neto@creathus.org.br>
2020-12-01 18:07:03 -0400

    Instances of Application

--- src/main/java/com/nando/Testes/video/aulas/TestesVideoAulasApplication.java
+++ src/main/java/com/nando/Testes/video/aulas/TestesVideoAulasApplication.java
@@ -1,13 +1,30 @@
 package com.nando.Testes.video.aulas;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class TestesVideoAulasApplication {
+import com.nando.Testes.video.aulas.domain.Category;
+import com.nando.Testes.video.aulas.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class TestesVideoAulasApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TestesVideoAulasApplication.class, args);
+	}
+	
+	public void run(String...args) throws Exception {
+
+		Category cat1 = new Category(null, "Information System");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1));
 	}
 
 }
